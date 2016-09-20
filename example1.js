a9f1a10f59a46544d27e799b76300e53430f812b
jonatanCB <jcaparrosboronat@gmail.com>
2016-09-20 16:53:02 +0200

    practica dia 1

--- example1.js
+++ example1.js
@@ -2,14 +2,21 @@
 
 var a = 3;
 var b = "dasdadw";
-var c = a*b;
-console.log(c);
+var r1 = a*b;
+console.log(r1);
 
+
+var c;
+var r3 = a+c;
+console.log(r3);
 
 var d ="12";
 console.log(a*d);
 console.log(a+d);
 console.log(a + +d);
 
-
+var value; //value = undefined;
+console.log(value);
+value = null;
+console.log(typeof value);
 
